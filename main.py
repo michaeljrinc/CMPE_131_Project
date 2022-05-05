eb3fe8a4a927afe57c195ee0ad1401962de68892
michaeljr.inc <estanillab@gmail.com>
2022-05-05 14:30:21 -0700

    test

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 #redirct and url_for allows redirect from a specific function
 #grab an html file and render it as ome page
 
 
 app = Flask(__name__) #create instance of flask web app
+app.secret_key = "password"
+
 
 #define how to access the certain page
 #@app.route("/") #default will go to home page. can define certain
@@ -21,6 +23,31 @@ def home(): #returns what is being displayed on the page using basic text
 def home(): #returns what is being displayed on the page using html file
     return render_template("home.html") #inline html
 
+@app.route("/login",methods = ['GET','POST'])
+def login():
+    if request.method == "POST": #will post the information to user
+        user = request.form["nm"] #nm is the dictionary and gets it from HTML file
+        
+        #stores session values
+        session["user"] = user
+        return redirect(url_for("user"))  
+    else:
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    #gets user session and checks if it exist
+    if "user" in session:
+        user = session["user"]
+        return render_template("profile.html",user)
+
+    else: 
+
+        #if already logged in, redirect to user page
+        if "user" in session:
+            return redirect(url_for("user"))
+        #will return to login if not found
+        return redirect(url_for("login"))
 
 
 @app.route("/admin") #example of redirect
@@ -39,6 +66,11 @@ def pillow():
 def user(name):
     return name + " is ugly too"
 '''
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None) #remove user from session
+    return redirect(url_for("login"))
 
 if __name__ == "main": #runs the app
     app.run(debug = True) #allows for changes to be seen without restarting the server
--- static/scripts/pillow.js
+++ static/scripts/pillow.js
@@ -1,5 +1,22 @@
+
+
+/*simple example of selecting h1 html header file and adding text context
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'HELLO!';
+*/
+
+/*Event Example
+-select html element
+-addEventListener = sets up a function that will be called (function) that will be called
+whenever specified event (click) is delivered to target
+-functions that are passed to addEventListener are called anonymous functions (in this example, function())
+    -can also replace function() with arrow funtion '() =>'
+
+document.querySelector('html').addEventListener('click',function(){
+    alert("Hi")
+})
+
+*/
 
 //let x --> declares a value x
 
@@ -9,3 +26,37 @@ can combine different types in an array and access it as a normal array
 let array = [1,'Bob','Steve',10];
 
 */ 
+
+/* changing pictures when clicked
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/pillow.jpg'){
+        myImage.setAttribute( 'src', 'images/noodle.jpg');
+    }
+    else{
+        myImage.setAttribute( 'src', 'images/pillow.jpg');
+    }
+}
+*/
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+function setUserName(){
+    let myName = prompt('Please enter your name'); //contains a prompt(displays a dialog box) 
+    localStorage.setItem('name', myName); //stores name in local storage
+    myHeading.textContent = 'Logged in as user: ' + myName;
+}
+
+if (!localStorage.getItem('name')){ //checks if stored in localStorage and if it isn't, call function to create it
+    setUserName();
+}
+else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Logged in as user: ' + myName;
+}
+
+myButton.onclick = function () {setUserName();}
